dcd3cfeab7f3d3e236746c597be430d2d118fced
Kamil Grondys <kamil.grondys@codecool.com>
2020-04-14 19:18:19 +0200

    init unittests
    Initializing unit tests for Zaawansowany project

---
+++ Zaawansowany/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace unittest_plus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
