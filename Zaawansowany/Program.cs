7f5fe6fd98588b282daf09269190bfda9b5cf651
Kamil Grondys <kamil.grondys@codecool.com>
2020-04-14 19:38:15 +0200

    finishing Zaawansowany project
    Adding NaPotrzebyWyjatku + error handling

--- Zaawansowany/Program.cs
+++ Zaawansowany/Program.cs
@@ -4,8 +4,21 @@ namespace unittest_plus
 {
     class Program
     {
+        static public int NaPotrzebyWyjatku(int a){
+            if(a==7) {
+                Console.WriteLine("jesteś szczęściarzem, bo nie zobaczysz wyjątku :)");
+                return 0;
+            }
+            throw new NotImplementedException("jeszcze nie zaimplementowałem");
+        }
+        
         static void Main(string[] args)
         {
+            try{
+                NaPotrzebyWyjatku(3);
+            } catch (Exception e){
+                Console.WriteLine("nie masz szczęścia, bo "+e.Message);
+            }
             Console.WriteLine("Hello World!");
         }
     }
